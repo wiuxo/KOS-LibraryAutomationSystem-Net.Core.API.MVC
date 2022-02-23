d39c315058e4e18d7039011644ec7fb8ebb2522b
Serdar İslamoğlu <Serdar_islamoglu@hotmail.com.tr>
2022-02-23 14:57:12 +0300

    v0.3

---
+++ KOS.Entities/DTOs/ErrorDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOS.Entities.DTOs
+{
+    public class ErrorDto
+    {
+        public string PropertyName { get; set; }
+        public string ErrorCode { get; set; }
+    }
+}
